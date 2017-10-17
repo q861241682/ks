e78c6db9cc6c405649e82d31e48baf269a0867d8
wj <715228289@qq.com>
2017-10-17 15:18:18 +0800

    第一个项目

---
+++ First/src/main/text1.java
@@ -0,0 +1,13 @@
+package main;
+
+public class text1 {
+
+	public void a(){
+		System.out.println("hello");
+	}
+	
+	public static void main(String[] args) {
+		text1 b = new text1();
+		b.a();
+	}
+}
